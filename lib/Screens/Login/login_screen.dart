03128068819b9c21c9f3cbe83e2dff102dfb24f9
IKENNA <ikenna@IKENNAs-MacBook-Pro.local>
2021-06-02 06:08:10 +0100

    Login screen

---
+++ lib/Screens/Login/login_screen.dart
@@ -0,0 +1,13 @@
+import 'package:auth_ui/Screens/Login/components/background.dart';
+import 'package:flutter/material.dart';
+import 'package:auth_ui/Screens/Login/components/body.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Background(),
+    );
+  }
+}
+
